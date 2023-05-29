d5ddfe0346882bd7817ee8fad91d1c87aae12e77
Jeffaaaaaah <jeffaaaaaah@gmail.com>
2023-05-29 17:56:57 -0400

    made fetch its own file

--- static/matchHistory.ts
+++ static/matchHistory.ts
@@ -1,3 +1,5 @@
+import { fetchMatchHistory } from "./fetch";
+
 var contentBody: HTMLElement | null;
 
 enum State {
@@ -25,28 +27,6 @@ async function process() {
     
 }
 
-
-function fetchMatchHistory(name: string | null): any { //python code already handles null input for player search
-    return fetch('/api/matchhistory',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 'playername': name }) //creates post body to be sent off
-    })
-    .then(response => { //checking for response error codes
-        if(!response.ok) {
-            throw new Error("response not ok");
-        }
-        return response.json(); //.json() returns a promise after it is finished parsing
-    })
-    .then(json => {
-        console.log(json);
-        return json;
-
-    })
-    .catch(e => console.log(e));
-} //fetchMatchHistory
 
 
 
